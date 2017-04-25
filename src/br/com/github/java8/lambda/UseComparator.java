51e594ffae9423868ebeabcd1df4a3a36fa7442d
Felipe Knappe <fknappe@gmail.com>
2017-04-25 00:27:52 -0300

    Added lambda and functional interface examples

--- src/br/com/github/java8/lambda/UseComparator.java
+++ src/br/com/github/java8/lambda/UseComparator.java
@@ -1,8 +1,30 @@
 package br.com.github.java8.lambda;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class UseComparator {
 
     public static void main(String[] args) {
 
+        List<String> strings = Arrays.asList(
+            "AAA", "bbb", "CCC", "dddd", "EEEE"
+        );
+
+        Collections.sort(strings);
+        System.out.println(String.format("Case sensitive sort: %s", strings));
+
+        /*Collections.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                return str1.compareToIgnoreCase(str2);
+            }
+        });*/
+
+        /*Collections.sort(strings, (str1, str2) -> str1.compareToIgnoreCase(str2));*/
+
+        Collections.sort(strings, String::compareToIgnoreCase);
+        System.out.println(String.format("Case insensitive sort: %s", strings));
     }
 }
