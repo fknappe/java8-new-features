838e424b04562d6ad2ffaf2de6d706af6bf5cacf
Felipe Knappe <fknappe@gmail.com>
2017-04-25 23:34:03 -0300

    Added static method reference example

---
+++ src/br/com/github/java8/methodreference/UseStaticReference.java
@@ -0,0 +1,22 @@
+package br.com.github.java8.methodreference;
+
+import br.com.github.java8.pojo.Person;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class UseStaticReference {
+
+    public static void main(String[] args) {
+
+        List<Person> people = Arrays.asList(
+                new Person("John Doe", 48),
+                new Person("Jane Doe",  35),
+                new Person("Peter Doe", 76)
+        );
+
+        Collections.sort(people, Person::compareAges);
+        people.forEach(System.out::println);
+    }
+}
