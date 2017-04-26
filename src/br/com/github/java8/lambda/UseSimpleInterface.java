04fb832b525dbb55d13ebdeddf0ae42f7a8a0c6e
Felipe Knappe <fknappe@gmail.com>
2017-04-25 23:32:29 -0300

    Removed useless comment on UseSimpleInterface example

--- src/br/com/github/java8/lambda/UseSimpleInterface.java
+++ src/br/com/github/java8/lambda/UseSimpleInterface.java
@@ -4,8 +4,7 @@ public class UseSimpleInterface {
 
 	public static void main(String[] args) {
 
-		// An instance of a functional interface using a java8 expression
-		SimpleInterface tester = () -> System.out.println("Java SE 8 is awesome!");
-		tester.test();
+		SimpleInterface simpleInterface = () -> System.out.println("Java 8 is awesome!");
+		simpleInterface.test();
 	}
 }
